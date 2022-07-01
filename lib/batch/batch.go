2b9a90a77d258933e81ba84a486215191b7ebdbf
Anton Aksiutsin <anak@NEWAPEX.NET>
2022-07-01 14:59:53 +0300

    Add Semaphore and Mutex

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,7 @@
 package batch
 
 import (
+	"sync"
 	"time"
 )
 
@@ -14,5 +15,22 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	sem := make(chan struct{}, pool)
+	var i int64
+	for i = 0; i < n; i++ {
+		wg.Add(1)
+		sem <- struct{}{}
+		go func(i int64) {
+			u := getOne(i)
+			mu.Lock()
+			res = append(res, u)
+			mu.Unlock()
+			<-sem
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+	return
 }
